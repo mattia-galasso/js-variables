0c653332ca64ef0e3b7e17968afbbf9847a75b16
MattiaGalasso <galasso.mattia94@gmail.com>
2025-12-15 14:52:05 +0100

    JavaScript Exercise Two

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -2,22 +2,40 @@
 // Crea una variabile chiamata name e assegnale il tuo nome.
 // Stampa il valore di nome nella console.
 
+const myName = "Mattia";
+console.log("Name:", myName);
+
 // 2. Tipi primitivi
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
 
-// 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
-// e le variabili sopra in modo da stampare correttamente 
-// in console la stringa con il nome completo
-const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; // NON MODIFICARE QUESTA RIGA
-const firstName = 'Jane'; // NON MODIFICARE QUESTA RIGA
-const lastName = 'Smith'; // NON MODIFICARE QUESTA RIGA
+const myAge = 31;
+const myLastName = "Galasso";
+const myChoice = true;
 
-const fullName = null;
+console.log(
+  "Age:",
+  myAge,
+  "|",
+  "LastName:",
+  myLastName,
+  "|",
+  "Choice:",
+  myChoice
+);
+
+// 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
+// e le variabili sopra in modo da stampare correttamente
+// in console la stringa con il nome completo
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // NON MODIFICARE QUESTA RIGA
+const firstName = "Jane"; // NON MODIFICARE QUESTA RIGA
+const lastName = "Smith"; // NON MODIFICARE QUESTA RIGA
+
+const fullName = firstName + " " + lastName;
 console.log(fullName); // Output: "Jane Smith"
 
 // 4. Imposta questa variabile utilizzando una proprietà
 // sulla variabile alphabet per ottenere la sua lunghezza
-const numberOfLettersInAlphabet = null;
+const numberOfLettersInAlphabet = alphabet.length;
 console.log(numberOfLettersInAlphabet); // Output: 26
